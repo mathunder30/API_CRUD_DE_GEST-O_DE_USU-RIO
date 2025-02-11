fc79f4a9226d15523fe3389fdba54de69be1fcfb
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-02-11 15:52:02 -0300

    criando um arquivo crud com metodo estatico onde cria, busca, atualiza e deleta as informações do usuario

--- src/DB.ts
+++ src/DB.ts
@@ -4,7 +4,7 @@ import mysql from 'mysql2';
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
-    port:'3306',
+    port: 3306,
     user: 'root',
     password: 'aluno',
     database: 'api_crud',
