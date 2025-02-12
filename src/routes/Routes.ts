5a39ec027c10ed5d5b23726d2b1d2936fd32d4db
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-02-12 11:55:17 -0300

    excluindo a rota routes .js

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,5 +1,5 @@
 import Express from "express";
-import { criandoUsario, buscandoTodosUsuarios, buscandoUsuarioId, atualizarUsuario,deletarUsuario } from "../controllers/UsuarioController";
+import { criandoUsario, buscandoTodosUsuarios, buscandoUsuarioId, atualizarUsuario, deletarUsuario } from "../controllers/UsuarioController";
 
 const router = Express.Router();
 
